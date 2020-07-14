45defc3088b66cec7fc9a31a4d31bcddc08feb55
Ella Mae Freshwater <emfresh2o@Ellas-MacBook-Pro.local>
2020-07-14 00:48:47 -0400

    more changes

--- js/index.js
+++ js/index.js
@@ -3,16 +3,16 @@
 //Event changes nav background -> lightgreen when mouseover, and back -> white when leaving
 let navSection = document.querySelector(".nav-container");
     navSection.addEventListener("mouseover", () => {
-        navSection.style.background = "lightgreen"
-    })
-    navSelector.addEventListener("mouseleave", () => {
-        navSelector.style.background = "white"})
+        navSection.style.background = "lightgreen"})
+        
+    navSection.addEventListener("mouseleave", () => {
+        navSection.style.background = "white"})
 
 //Event changes main bus image when clicked
 let busImage = document.querySelector(".intro img")
     busImage.addEventListener("click", () => {
-        busImage.src = "img/funbustour.png"
-      })
+        busImage.src = "img/funbustour.png"})
+
 //Event changes "Adventure" image to new image when clicked
 let adventureImg = document.querySelector(".img-content img")
     adventureImg.addEventListener("click", () => {
@@ -23,6 +23,28 @@ let destinationImg = document.querySelector(".img-content img")
     destinationImg.addEventListener("dblclick", () => {
         destinationImg.src = "img/islandBoat.png"})
 
+// Make all p tags change to blue on 'keydown'
+const p = document.querySelectorAll('p');
+p.forEach(element => {
+    window.addEventListener('keydown', () => {
+        element.style.color = '#17A2B8';
+    })
+})
+// Make all p tags change to black on 'keyup'
+p.forEach(element => {
+    window.addEventListener('keyup', () => {
+        element.style.color = 'black';
+    })
+})
+
+// Event change all h2 tags to blue 
+const title = document.querySelectorAll('h2');
+title.forEach(element => {
+    window.addEventListener('contextmenu', () => {
+        element.style.color = '#17A2B8';
+    })
+})
+        
 //Event change "SignUp Button" with alerts
 
 const signUpBtn1 = document.querySelector('#btn-1');
